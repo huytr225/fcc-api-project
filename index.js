a1b1d8531f5dfaaa72b498824f39a9440638b0ae
Huy <huytr225@gmail.com>
2016-10-09 10:51:55 +0000

    third commit

--- index.js
+++ index.js
@@ -16,10 +16,25 @@ app.get('/api/:date', function (req, res) {
     res.json(ts.timestamp(req.params.date));
 });
 
-/*app.get('/new/:url', function(req, res) {
+app.get('/new/:url', function(req, res) {
+    function validURL(str) {
+        var expression = /https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,}/gi;
+        var regex = new RegExp(expression);
+        return str.match(regex);
+    }
     var url = req.params.url;
-    res.redirect('http://google.com');
-});*/
+    var response = {};
+    console.log(validURL(url));
+    if(validURL(url) == null){
+        response.error = "Wrong url format, make sure you have a valid protocol and real site.";
+        res.json(response);
+    }
+    else {
+        response['original_url'] = url;
+        res.json(response);
+    }
+    //res.redirect('http://google.com');
+});
 
 app.get('*', function(req, res) {
     res.send("Invalid page");
